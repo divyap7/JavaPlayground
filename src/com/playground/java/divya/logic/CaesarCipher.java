7e7ca22788fa12b6c4108495dc071713890a7f26
divyap7 <divyaprem25@gmail.com>
2018-05-19 23:42:54 +0530

    #6 bug fixed

--- src/com/playground/java/divya/logic/CaesarCipher.java
+++ src/com/playground/java/divya/logic/CaesarCipher.java
@@ -1,18 +1,29 @@
 package com.playground.java.divya.logic;
 
 public class CaesarCipher {
-    private void cipher(String word) {
-        int length = word.length();
-        for (int i = 0; i < length; i++) {
-            char character = word.charAt(i);
-            character = (char) (character + 3);
-            System.out.print(character);
+    private void encode(String word)
+
+    {
+        String encrypted = "";
+        for (int i = 0; i < word.length(); i++) {
+            int letter = word.charAt(i);
+            if (Character.isUpperCase(letter)) {
+                letter = letter + 3;
+                if (letter > 'Z')
+                    letter = letter - 26;
+            } else if (Character.isLowerCase(letter)) {
+                letter = letter + 3;
+                if (letter > 'z')
+                    letter = letter - 26;
+            }
+            encrypted += (char) letter;
         }
+        System.out.print(encrypted);
     }
 
     public static void main(String[] args) {
-        CaesarCipher cipher=new CaesarCipher();
-        cipher.cipher("ABCDEF");
+        CaesarCipher cipher = new CaesarCipher();
+        cipher.encode("xyz");
 
     }
 }
