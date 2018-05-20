08d5efa7ef75362680cba8fc11e38b67422a851a
divyap7 <divyaprem25@gmail.com>
2018-05-20 21:38:32 +0530

    #5 Changed according to comments

--- src/com/playground/java/divya/logic/Sequences.java
+++ src/com/playground/java/divya/logic/Sequences.java
@@ -13,24 +13,18 @@ public class Sequences {
     private void sequence2(int n) {
         for (int i = n; n >= 1; i--) {
             System.out.print(i);
+            n = n - 1;
             for (int j = 1; j < i; j++) {
                 System.out.print(i);
             }
-            n = n - 1;
         }
     }
 
 
-    private void sequence3() {
-        int n = 100;
-
+    private void sequence3(int n) {
         for (int i = 1; i <= n; i++) {
-            System.out.print(i);
-            System.out.print("-");
-            System.out.print(n);
-            System.out.print("-");
+            System.out.print(i +"-"+n+"-");
             n--;
-
         }
     }
 
@@ -45,7 +39,7 @@ public class Sequences {
 
     public static void main(String[] args) {
         Sequences sequence = new Sequences();
-        sequence.sequence3();
+        sequence.sequence3(100);
         System.out.println();
         sequence.sequence4(5);
         System.out.println();
